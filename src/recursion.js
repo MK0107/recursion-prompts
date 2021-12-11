a554458340e3c3f57aac0567ae5e98cfd489dce5
Minseo Kim <minseo0321@gmail.com>
2021-12-10 17:10:39 -0800

    problem 10 completed

--- src/recursion.js
+++ src/recursion.js
@@ -131,6 +131,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+
+  if (string.length < 2) {
+    return true;
+  }
+
+  if (string[0] === string[string.length -1]) {
+    return palindrome(string.slice(1, string.length -1));
+  }
+
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
